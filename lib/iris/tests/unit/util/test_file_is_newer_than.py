f6011c746cea3cd3707c9584e754ab940a69163f
jaddix <SgtVarmint2112@protonmail.com>
2026-05-08 11:52:27 -0400

    pathlib migration

--- lib/iris/tests/unit/util/test_file_is_newer_than.py
+++ lib/iris/tests/unit/util/test_file_is_newer_than.py
@@ -5,7 +5,6 @@
 """Test function :func:`iris.util.test_file_is_newer`."""
 
 import os
-import os.path
 
 import pytest
 
@@ -17,7 +16,7 @@ class TestFileIsNewer:
 
     def _name2path(self, filename):
         """Add the temporary dirpath to a filename to make a full path."""
-        return os.path.join(self.temp_dir, filename)
+        return str(self.temp_dir / filename)
 
     @pytest.fixture(autouse=True)
     def _setup(self, tmp_path):
