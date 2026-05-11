3de594e2eca085fbc30f3b8806c174b57f47dd9d
jaddix <SgtVarmint2112@protonmail.com>
2026-05-11 13:37:37 -0400

    migrated cf to use pathlib.path

--- lib/iris/fileformats/cf.py
+++ lib/iris/fileformats/cf.py
@@ -21,7 +21,6 @@ References
 
 from abc import ABCMeta, abstractmethod
 from collections.abc import Iterable, MutableMapping
-import os
 from pathlib import Path
 import re
 from typing import ClassVar, Optional
@@ -1366,7 +1365,9 @@ class CFReader:
         self._own_file = False
         if isinstance(file_source, str):
             # Create from filepath : open it + own it (=close when we die).
-            self._filename = os.path.expanduser(file_source)
+            self._filename = str(Path(file_source).expanduser())
+            if file_source.startswith("https:"):
+                self._filename = file_source
             self._dataset = _thread_safe_nc.DatasetWrapper(self._filename, mode="r")
             self._own_file = True
         else:
